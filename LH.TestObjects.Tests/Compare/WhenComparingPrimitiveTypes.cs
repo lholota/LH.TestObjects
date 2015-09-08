ae4accdc01d19599bae8c49a0085c0984121f31a
LH <l.holota@outlook.com>
2015-09-08 12:55:21 +0200

    Tests for comparing primitive types directly added, closes #45

---
+++ LH.TestObjects.Tests/Compare/WhenComparingPrimitiveTypes.cs
@@ -0,0 +1,30 @@
+﻿namespace LH.TestObjects.Tests.Compare
+{
+    using System.Linq;
+    using FluentAssertions;
+    using NUnit.Framework;
+    using TestObjects.Compare;
+
+    [TestFixture]
+    public class WhenComparingPrimitiveTypes
+    {
+        [Test]
+        public void ThenShouldPassWhenComparingTwoEqualStrings()
+        {
+            var comparator = new ObjectComparator<string>();
+            var result = comparator.Compare("AAA", "AAA");
+
+            result.AreSame.Should().BeTrue();
+        }
+
+        [Test]
+        public void ThenShouldFailWhenComparingTwoDifferentStrings()
+        {
+            var comparator = new ObjectComparator<string>();
+            var result = comparator.Compare("AAA", "BBB");
+
+            result.AreSame.Should().BeFalse();
+            result.Differences.Single().PropertyPath.Should().BeNullOrEmpty();
+        }
+    }
+}
