b0ba3637cc002ab21b3bbcaa98ee639343ddf131
LH <l.holota@outlook.com>
2015-09-10 14:46:13 +0200

    Direct array comparison tests added, closes #46

--- LH.TestObjects.Tests/Compare/WhenComparingPrimitiveTypes.cs
+++ LH.TestObjects.Tests/Compare/WhenComparingPrimitiveTypes.cs
@@ -26,5 +26,30 @@
             result.AreSame.Should().BeFalse();
             result.Differences.Single().PropertyPath.Should().BeNullOrEmpty();
         }
+
+        [Test]
+        public void ThenShouldSucceedIfCollectionsAreEqual()
+        {
+            var objA = new[] {"AAA", "BBB"};
+            var objB = new[] {"AAA", "BBB"};
+
+            var comparator = new ObjectComparator<string[]>();
+            var result = comparator.Compare(objA, objB);
+
+            result.AreSame.Should().BeTrue();
+        }
+
+        [Test]
+        public void ThenShouldSucceedIfCollectionsDiffer()
+        {
+            var objA = new[] { "AAA", "BBB" };
+            var objB = new[] { "AAA", "CCC" };
+
+            var comparator = new ObjectComparator<string[]>();
+            var result = comparator.Compare(objA, objB);
+
+            result.AreSame.Should().BeFalse();
+            result.Differences.Single().PropertyPath.Should().Be("[1]");
+        }
     }
 }
