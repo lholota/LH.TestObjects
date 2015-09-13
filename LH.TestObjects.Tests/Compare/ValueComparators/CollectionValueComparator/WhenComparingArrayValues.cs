26d6f512086ce2bb4a3ca279c2e6d2bf12da6541
LH <l.holota@outlook.com>
2015-09-13 14:12:47 +0200

    Jagged arrays test added, closes #67

--- LH.TestObjects.Tests/Compare/ValueComparators/CollectionValueComparator/WhenComparingArrayValues.cs
+++ LH.TestObjects.Tests/Compare/ValueComparators/CollectionValueComparator/WhenComparingArrayValues.cs
@@ -2,7 +2,9 @@
 {
     using System.Linq;
     using Domain;
+    using FluentAssertions;
     using NUnit.Framework;
+    using TestObjects.Compare;
 
     [TestFixture]
     public class WhenComparingArrayValues : WhenComparingCollectionValuesBase<SimpleDomain[]>
@@ -35,6 +37,59 @@
                 input[2],
                 input[1]
             };
+        }
+
+        [Test]
+        public void ThenShouldSuccedWhenComparingEqualJaggedArrays()
+        {
+            var objA = new GenericDomain<int[][]> { GenericProp = this.CreateJaggedArray() };
+            var objB = new GenericDomain<int[][]> { GenericProp = this.CreateJaggedArray() };
+
+            var comparator = new ObjectComparator<GenericDomain<int[][]>>();
+            var result = comparator.Compare(objA, objB);
+
+            result.AreSame.Should().BeTrue();
+        }
+
+        [Test]
+        public void ThenShouldSuccedWhenComparingJaggedArraysWithDifferentValues()
+        {
+            var objA = new GenericDomain<int[][]> { GenericProp = this.CreateJaggedArray() };
+            var objB = new GenericDomain<int[][]> { GenericProp = this.CreateJaggedArray() };
+
+            objA.GenericProp[0][1] = 5;
+            objB.GenericProp[0][1] = 10;
+
+            var comparator = new ObjectComparator<GenericDomain<int[][]>>();
+            var result = comparator.Compare(objA, objB);
+
+            result.AreSame.Should().BeFalse();
+            result.Differences.Single().PropertyPath.Should().Be("GenericProp[0][1]");
+        }
+
+        [Test]
+        public void ThenShouldSuccedWhenComparingJaggedArraysWithDifferentLengths()
+        {
+            var objA = new GenericDomain<int[][]> { GenericProp = this.CreateJaggedArray() };
+            var objB = new GenericDomain<int[][]> { GenericProp = this.CreateJaggedArray() };
+
+            objA.GenericProp[0] = new int[3];
+            objB.GenericProp[0] = new int[5];
+
+            var comparator = new ObjectComparator<GenericDomain<int[][]>>();
+            var result = comparator.Compare(objA, objB);
+
+            result.AreSame.Should().BeFalse();
+            result.Differences.Single().PropertyPath.Should().Be("GenericProp[0]");
+        }
+
+        private int[][] CreateJaggedArray()
+        {
+            var result = new int[2][];
+            result[0] = new[] { 1, 2, 3 };
+            result[1] = new[] { 4 };
+
+            return result;
         }
     }
 }
